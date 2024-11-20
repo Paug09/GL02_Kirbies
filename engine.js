75203c47c9a9c3740ad713bae106efac1568e0c4
Paug09 <paul.fernandez0902@gmail.com>
2024-11-20 18:50:30 +0100

    Merge branche bleu

--- engine.js
+++ engine.js
@@ -1,6 +1,7 @@
 var Engine = {
     check: function () {
         console.log("Engine Ready !")
+        console.log("Blue branch validated")
         return true
     }
 }
--- radio.js
+++ radio.js
@@ -1,6 +1,7 @@
 var Radio = {
     check: function () {
         console.log("Radio Ready !")
+        console.log("Blue branch validated")
         return true
     }
 }
