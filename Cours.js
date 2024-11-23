f62e1e4d64afc3a1e01d28815969b7c7f437f076
Paug09 <paul.fernandez0902@gmail.com>
2024-11-23 18:05:40 +0100

    Fichier définition slot

---
+++ Cours.js
@@ -0,0 +1,9 @@
+var Slot = function(C,T,P,H,G,S) {
+    this.cours = C;
+    this.type = T;
+    this.capacite = P;
+    this.heure = H;
+    this.groupe = G;
+    this.salle = S;
+}
+module.exports = Slot;
