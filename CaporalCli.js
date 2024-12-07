d89d0709e250e8e26e84bbb97985e415fad02dfa
Iris <noctaly98@gmail.com>
2024-12-07 01:03:56 +0100

    Spec 3
    Pas sûre que ça fonctionne mais c'est un début

--- CaporalCli.js
+++ CaporalCli.js
@@ -23,7 +23,7 @@ cli
 				return logger.warn(err);
 			}
 
-			var analyzer = new CruParser(options.showTokenize, options.showSymbols,options.showDebug);
+			var analyzer = new CruParser(options.showTokenize, options.showSymbols, options.showDebug);
 			analyzer.parse(data);
 
 			if (analyzer.errorCount === 0) {
@@ -46,15 +46,111 @@ cli
 	})
 
 	//readme
-    .command('readme', 'Display the README.txt file')
-    .action(({logger}) => {
-        fs.readFile("./README.txt", 'utf8', function (err,data) {
-            if (err) {
-                return logger.warn(err);
-            }
-            logger.info(data);
-        });
-    })
+	.command('readme', 'Display the README.txt file')
+	.action(({ logger }) => {
+		fs.readFile("./README.txt", 'utf8', function (err, data) {
+			if (err) {
+				return logger.warn(err);
+			}
+			logger.info(data);
+		});
+	})
+
+	.command('check', 'Check if <file> is a valid Cru file')
+	.argument('<file>', 'The file to check with Cru parser')
+	.option('-s, --showSymbols', 'log the analyzed symbol at each step', { validator: cli.BOOLEAN, default: false })
+	.option('-t, --showTokenize', 'log the tokenization results', { validator: cli.BOOLEAN, default: false })
+	.option('-d, --showDebug', 'log the debug information', { validator: cli.BOOLEAN, default: false })
+	.action(({ args, options, logger }) => {
+
+		fs.readFile(args.file, 'utf8', function (err, data) {
+			if (err) {
+				return logger.warn(err);
+			}
+
+			var analyzer = new CruParser(options.showTokenize, options.showSymbols, options.showDebug);
+			analyzer.parse(data);
+
+			if (analyzer.errorCount === 0) {
+				logger.info("The .cru file is a valid cru file".green);
+			} else {
+				logger.info("The .cru file contains error".red);
+				//Donne le nombre d'erreurs
+				logger.info("Error count : %d", analyzer.errorCount);
+			}
+			analyzer.ParsedCourse.forEach(course => {
+				console.log(`Course Code: ${course.courseCode}`);
+				course.timeSlots.forEach((ts, index) => {
+					console.log(`- Time Slot ${index + 1}: ${ts.toString()}`);
+				});
+			});
+			logger.debug(analyzer.parsedCourse);
+
+		});
+
+	})
+
+	//readme
+	.command('readme', 'Display the README.txt file')
+	.action(({ logger }) => {
+		fs.readFile("./README.txt", 'utf8', function (err, data) {
+			if (err) {
+				return logger.warn(err);
+			}
+			logger.info(data);
+		});
+	})
+
+	// search when a room is free
+	.command('whenFreeRoom', 'Check when a certain room is free during the week')
+	.argument('<file>', 'The Cru file to search')
+	.argument('<room>', 'The room you want to search if it is free')
+	.action(({ args, logger }) => {
+		fs.readFile(args.file, 'utf8', function (err, data) {
+			if (err) {
+				return logger.warn(err);
+			}
+
+			const analyzer = new CruParser();
+			analyzer.parse(data);
+
+			//if (analyzer.errorCount === 0) {
+			// find the given room in the database
+			const roomToSearch = analyzer.ParsedCourse.find(room => room.timeSlots === args.room);
+
+
+			// if the room is found
+			if (roomToSearch) {
+				// Trouver les moments où est occupée la salle
+				const occupiedSlots = [];
+				const dayMapping = { "L": 1, "MA": 2, "ME": 3, "J": 4, "V": 5, "S": 6 };
+
+				room.timeSlots.forEach(slot => {
+					const dayOfWeek = dayMapping[slot.heure.split(" ")[0]]; // Récupère le jour
+					const timeRange = slot.heure.split(" ")[1]; // Récupère "08:00-10:00"
+					occupiedSlots.push(slot.salle); //Ajoute le crénaux au tableau des crénaux occupés
+					console.log(`La salle est occupée durant le crénau suivant: ${slot.horaire}`);
+				});
+
+				// Calculer les moments où la salle est disponible
+				const availabilityRoom = allFreeMoment.filter(slot => !occupiedSlots.includes(slot));
+
+				if (availabilityRoom.length > 0) {
+					logger.info(`The room ${args.room} is available on the following slots:`);
+					availabilityRoom.forEach(slot => logger.info(`- ${slot}`));
+				} else {
+					logger.warn(`The room ${args.room} does not have any available slots.`);
+				}
+			} else {
+				logger.warn("No rooms found for the given name.");
+			}
+
+			//} else {
+			//	logger.warn("The .cru file contains parsing errors.");
+			//}
+		});
+	});
+
 /*
 	// search
 	.command('search', 'Free text search on POIs\' name')
@@ -141,25 +237,25 @@ cli
 				});
 
 				/* Canvas version */
-				/*
-				var runtime = vg.parse(myChart);
-				var view = new vg.View(runtime).renderer('canvas').background("#FFF").run();
-				var myCanvas = view.toCanvas();
-				myCanvas.then(function(res){
-					fs.writeFileSync("./result.png", res.toBuffer());
-					view.finalize();
-					logger.info(myChart);
-					logger.info("Chart output : ./result.png");
-				})			
-				
+/*
+var runtime = vg.parse(myChart);
+var view = new vg.View(runtime).renderer('canvas').background("#FFF").run();
+var myCanvas = view.toCanvas();
+myCanvas.then(function(res){
+	fs.writeFileSync("./result.png", res.toBuffer());
+	view.finalize();
+	logger.info(myChart);
+	logger.info("Chart output : ./result.png");
+})			
+	
 
 
-			} else {
-				logger.info("The .vpf file contains error".red);
-			}
+} else {
+logger.info("The .vpf file contains error".red);
+}
 
-		});
-	}) */
+});
+}) */
 
 
 
